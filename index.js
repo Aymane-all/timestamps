855afd2341ca526142033d107ede7003909d0807
Aymane Abbad <aymaneel560@gmail.com>
2025-06-29 00:40:49 +0100

    faire timestamp

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 
-
+var cors = require('cors');
+app.use(cors());
 
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/views/index.html')
 });
-
-let listener = app.listen(process.env.Port || 3000, () => {
-    console.log('Server is running on port 3000' + listener.address().port);
+app.listen(process.env.Port || 3001 , () => {
+    console.log('Server is running on port 3001' );
 });
 
+//  listener.address().port;  
+
+
+app.get('/api/timestamp/:date', (req, res) => {
+   let date ;
+   dateparam = new Date(req.params.date);
+
+   if (!dateparam) {
+    date = new Date();
+   } else {
+    date = new Date(parseInt(dateparam));
+   }
+   res.json({unix: date.getTime(), utc: date.toUTCString()});
+})
