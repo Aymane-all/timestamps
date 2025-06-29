657b41cddabedd844aa7cf50d5669f5d851afcaf
Aymane Abbad <aymaneel560@gmail.com>
2025-06-29 01:12:16 +0100

    faire exercice timestamp

--- index.js
+++ index.js
@@ -10,29 +10,34 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-
 app.get('/api/:date?', (req, res) => {
     let dateParam = req.params.date;
     let date;
 
-    if (!dateParam) {
-        date = new Date(); 
+   
+    if (!dateParam || dateParam === '' || dateParam === undefined) {
+        date = new Date();
     } else {
+        
         if (!isNaN(Number(dateParam))) {
             date = new Date(Number(dateParam));
         } else {
+            
             date = new Date(dateParam);
         }
     }
 
-    if (date.toString() === 'Invalid Date') {
+    
+    if (date.toString() === 'Invalid Date' || isNaN(date.getTime())) {
         return res.json({ error: 'Invalid Date' });
     }
 
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  
+    res.json({ 
+        unix: date.getTime(), 
+        utc: date.toUTCString() 
+    });
 });
-
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
